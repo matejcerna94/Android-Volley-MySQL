e19d297cdcd4f961b470a67bd705cbb49106b5dd
Mihael <matejcerna94@gmail.com>
2020-04-14 21:20:27 +0200

    Dialog working correctly

--- app/src/main/java/com/matejcerna/tabapplication/ExampleDialog.java
+++ app/src/main/java/com/matejcerna/tabapplication/ExampleDialog.java
@@ -1,8 +1,6 @@
 package com.matejcerna.tabapplication;
 
 import android.app.Dialog;
-import android.content.DialogInterface;
-import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -11,25 +9,40 @@ import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
+
 public class ExampleDialog extends DialogFragment {
 
-    TextView textViewItemNameDialog;
-    TextView textViewItemPriceDialog;
-    ImageView imageViewItemImageDialog;
+
+    @BindView(R.id.item_name)
+    TextView itemName;
+    @BindView(R.id.item_image)
+    ImageView itemImage;
+    @BindView(R.id.item_price)
+    TextView itemPrice;
+    Unbinder unbinder;
+
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
         LayoutInflater inflater = getActivity().getLayoutInflater();
         View view = inflater.inflate(R.layout.my_dialog, null);
-        textViewItemNameDialog = view.findViewById(R.id.text_view_item_name_dialog);
+        ButterKnife.bind(this, view);
+
+
+       /* textViewItemNameDialog = view.findViewById(R.id.text_view_item_name_dialog);
         textViewItemPriceDialog = view.findViewById(R.id.text_view_item_price_dialog);
-        imageViewItemImageDialog = view.findViewById(R.id.image_view_item_image_dialog);
+        imageViewItemImageDialog = view.findViewById(R.id.image_view_item_image_dialog);*/
 
         Bundle bundle = getArguments();
         String item_id = bundle.getString("item_id");
@@ -37,24 +50,23 @@ public class ExampleDialog extends DialogFragment {
         String item_image = bundle.getString("item_image");
         Log.d("image dialog", item_image);
         String item_price = bundle.getString("item_price");
-        textViewItemNameDialog.setText(item_name);
-        textViewItemPriceDialog.setText(item_price);
-        Picasso.get().load(item_image).fit().centerCrop().into(imageViewItemImageDialog);
+        itemName.setText(item_name);
+        itemPrice.setText(item_price);
+        Picasso.get().load(item_image).fit().centerInside().into(itemImage);
 
-        builder.setView(view).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-
-            }
-        }).setPositiveButton("Order", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-
-            }
-        });
+        builder.setView(view);
 
         return builder.create();
 
 
+    }
+
+
+
+
+
+    @OnClick(R.id.image_view_close)
+    public void onViewClicked() {
+        dismiss();
     }
 }
